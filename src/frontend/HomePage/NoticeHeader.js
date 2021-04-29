0ab0cbb0d0d6e3a4a36ae3d343fd994f2acb781b
m4mohammadkhalid <61066087+m4mohammadkhalid@users.noreply.github.com>
2021-04-29 08:18:30 +0530

    update

--- src/frontend/HomePage/NoticeHeader.js
+++ src/frontend/HomePage/NoticeHeader.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import Marquee from 'react-double-marquee';
 
 const NoticeHeader = () => {
     return (
-        <div className="news_ticker bg-warning">
-  <div className="container">
-    <marquee behavior="scroll" direction="left" onMouseOver="this.stop();" onMouseOut="this.start();">
-      <Link to="#" target="_blank">Universities the winners if Labor assumes government</Link> ||
-      <Link to="#" target="_blank">Overseas students are driving rapid population growth</Link> || 
-      <Link to="#" target="_blank">Admission for Ph.D. Programme 2019-20</Link> || 
-      <Link to="#" target="_blank">Application form for Provisional Registration for Ph.D</Link> || 
-      <Link to="#" target="_blank">Graduates need second degree to become a high earner</Link> ||
-      <Link to="#" target="_blank">Eduglobal Portal for students seeking Education Loan</Link> || 
-      <Link to="#" target="_blank">Creating inclusive curricula in higher education</Link>
-    </marquee>
+        <>
+           <div style={{backgroundColor:'#1E2264', color:'white'}}>
+  <div style={{
+        width: '100%',
+        whiteSpace: 'nowrap',
+      }} className="container">
+    <Marquee behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
+      <Link style={{color:'white'}} to="#" target="_blank">Universities the winners if Labor assumes government</Link> || 
+      <Link style={{color:'white'}} to="#" target="_blank"> Overseas students are driving rapid population growth</Link> ||  
+      <Link style={{color:'white'}} to="#" target="_blank"> Admission for Ph.D. Programme 2019-20</Link> || 
+      <Link style={{color:'white'}} to="#" target="_blank"> Application form for Provisional Registration for Ph.D</Link> ||  
+      <Link style={{color:'white'}} to="#" target="_blank"> Graduates need second degree to become a high earner</Link> || 
+      <Link style={{color:'white'}} to="#" target="_blank"> Eduglobal Portal for students seeking Education Loan</Link> ||  
+      <Link style={{color:'white'}} to="#" target="_blank"> Creating inclusive curricula in higher education</Link>
+    </Marquee>
   </div>
 </div>
 
+        </>
     )
 }
 
